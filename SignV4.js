78223de6888095f0365a3f6497eaf98994450051
İren SALTALI <iren@saltali.com>
2019-12-16 12:50:54 +0300

    PutRecordBatch is added

--- SignV4.js
+++ SignV4.js
@@ -3,7 +3,7 @@ const v4 = require('aws-signature-v4');
 const fs = require("fs");
 
 class SignV4 {
-    SignFirehosePost(date, uri, payload, AWSAccessKey, AWSSecret, Region) {
+    SignFirehosePostPutRecord(date, uri, payload, AWSAccessKey, AWSSecret, Region) {
         let service = "firehose";
 
         //Canonical Request
@@ -13,6 +13,37 @@ class SignV4 {
         let CanonicalHeaders = "host:" + uri.hostname + "\n"
             + "x-amz-date:" + this.yyyyMMddTHHmmssZ(date) + "\n"
             + "x-amz-target:Firehose_20150804.PutRecord\n";
+        let SignedHeaders = "host;x-amz-date;x-amz-target";
+        let HashedPayload = sha256(payload);
+
+        var CanonicalRequest = HTTPMethod + CanonicalURI + CanonicalQuerystring + CanonicalHeaders + "\n" + SignedHeaders + "\n" + HashedPayload;
+
+        //StringToSign
+        let StringToSignStart = "AWS4-HMAC-SHA256" + "\n";
+        let TimeStamp = this.yyyyMMddTHHmmssZ(date) + "\n";
+        let Scope = this.yyyyMMdd(date) + "/" + Region + "/" + service + "/aws4_request" + "\n";
+
+        var StringToSign = StringToSignStart + TimeStamp + Scope + sha256(CanonicalRequest);
+
+        //Signature
+        var signature = v4.createSignature(AWSSecret, date, Region, service, StringToSign);
+
+        //AuthorizationHeader
+        let AuthorizationHeader = "AWS4-HMAC-SHA256 Credential=" + AWSAccessKey + "/" + this.yyyyMMdd(date) + "/" + Region + "/" + service + "/aws4_request,"
+            + " SignedHeaders=" + SignedHeaders + ", Signature=" + signature;
+
+        return AuthorizationHeader;
+    }
+    SignFirehosePostPutRecordBatch(date, uri, payload, AWSAccessKey, AWSSecret, Region) {
+        let service = "firehose";
+
+        //Canonical Request
+        let HTTPMethod = "POST" + "\n";
+        let CanonicalURI = uri.pathname + "\n";
+        let CanonicalQuerystring = "" + "\n";
+        let CanonicalHeaders = "host:" + uri.hostname + "\n"
+            + "x-amz-date:" + this.yyyyMMddTHHmmssZ(date) + "\n"
+            + "x-amz-target:Firehose_20150804.PutRecordBatch\n";
         let SignedHeaders = "host;x-amz-date;x-amz-target";
         let HashedPayload = sha256(payload);
 
--- index.js
+++ index.js
@@ -4,54 +4,114 @@ const SignV4 = require("./SignV4.js");
 var signV4 = new SignV4();
 
 
-
-var uri = new URL('', 'https://firehose.eu-west-1.amazonaws.com');
-
-var data =  JSON.stringify({ test: 'tesdata' });
-var buff = new Buffer(data);
-var base64data = buff.toString('base64');
-var payload = JSON.stringify({
-    DeliveryStreamName: 'i4io-log-test-2',
-    Record: {
-      Data: base64data
-    }
-  });
-
-var Region = "eu-west-1";
-var date = new Date();
-
 var file = fs.readFileSync("AWS.key", "UTF-8");
 var AWSAccessKey = file.split(',')[0];
 var AWSSecret = file.split(',')[1];
+var uri = new URL('', 'https://firehose.eu-west-1.amazonaws.com');
+var Region = "eu-west-1";
+var date = new Date();
 
-var authHeader = signV4.SignFirehosePost(date, uri, payload, AWSAccessKey, AWSSecret, Region);
+PutRecord();
+PutRecords();
+
+function PutRecord() {
+    var data = JSON.stringify({
+        test: 'tesdata'
+    });
+    var buff = new Buffer(data);
+    var base64data = buff.toString('base64');
+    var payload = JSON.stringify({
+        DeliveryStreamName: 'i4io-log-test-2',
+        Record: {
+            Data: base64data
+        }
+    });
 
 
-var options = {
-    url: 'https://firehose.eu-west-1.amazonaws.com',
-    headers: {
-        "host": uri.hostname,
-        "x-amz-date": signV4.yyyyMMddTHHmmssZ(date),
-        "x-amz-target": "Firehose_20150804.PutRecord",
-        "Authorization": authHeader,
-        "Content-Type": "application/x-amz-json-1.1",
-        "Content-Length": Buffer.byteLength(payload, 'utf8'),
-        "Connection": "Keep-Alive"
-    },
-    body: payload
-};
+    var authHeader = signV4.SignFirehosePostPutRecord(date, uri, payload, AWSAccessKey, AWSSecret, Region);
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log("200");
-        var info = JSON.parse(body);
-        console.log(info);
+
+    var options = {
+        url: 'https://firehose.eu-west-1.amazonaws.com',
+        headers: {
+            "host": uri.hostname,
+            "x-amz-date": signV4.yyyyMMddTHHmmssZ(date),
+            "x-amz-target": "Firehose_20150804.PutRecord",
+            "Authorization": authHeader,
+            "Content-Type": "application/x-amz-json-1.1",
+            "Content-Length": Buffer.byteLength(payload, 'utf8'),
+            "Connection": "Keep-Alive"
+        },
+        body: payload
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("200");
+            var info = JSON.parse(body);
+            console.log(info);
+            console.log(body);
+        }
+        console.log(error);
         console.log(body);
     }
-    console.log(error);
-    console.log(body);
+
+    request.post(options, callback);
 }
 
-request.post(options, callback);
+function PutRecords() {
+    var payloadData = {
+        DeliveryStreamName: 'i4io-log-test-2',
+        Records: []
+    };
+
+    var buff = new Buffer("");
+    for (i = 0; i < 10; i++) {
+        console.log(i);
+        var data = JSON.stringify({
+            test: 'tesdata' + i
+        });
+        buff = Buffer.from(data);
+        var base64data = buff.toString('base64');
+
+        payloadData.Records.push({
+            Data: base64data
+        })
+    }
+
+    var payload = JSON.stringify(payloadData);
+    console.log(payload);
 
 
+
+
+    var authHeader = signV4.SignFirehosePostPutRecordBatch(date, uri, payload, AWSAccessKey, AWSSecret, Region);
+
+
+    var options = {
+        url: 'https://firehose.eu-west-1.amazonaws.com',
+        headers: {
+            "host": uri.hostname,
+            "x-amz-date": signV4.yyyyMMddTHHmmssZ(date),
+            "x-amz-target": "Firehose_20150804.PutRecordBatch",
+            "Authorization": authHeader,
+            "Content-Type": "application/x-amz-json-1.1",
+            "Content-Length": Buffer.byteLength(payload, 'utf8'),
+            "Connection": "Keep-Alive"
+        },
+        body: payload
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("200");
+            var info = JSON.parse(body);
+            console.log(info);
+            console.log(body);
+        }
+        console.log(error);
+        console.log(body);
+    }
+
+    request.post(options, callback);
+}
